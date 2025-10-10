83932a9f634878b08d85b1977b9a282976b3b486
telcaslor <thatbytc@gmail.com>
2025-10-10 13:30:52 +0200

    FIX error con constraints enfxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module es.potter {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens es.potter to javafx.fxml;
+    opens es.potter to javafx.fxml, javafx.base;
+
     exports es.potter;
 }
