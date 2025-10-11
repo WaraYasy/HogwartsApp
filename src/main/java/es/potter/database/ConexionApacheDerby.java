764621ce9e773d18136ee4e5ae4a2e03c992b304
WaraYasy <152279539+WaraYasy@users.noreply.github.com>
2025-10-11 11:26:35 +0200

    STYLE: Corregir typo en mensaje por consola

--- src/main/java/es/potter/database/ConexionApacheDerby.java
+++ src/main/java/es/potter/database/ConexionApacheDerby.java
@@ -88,7 +88,7 @@ public class ConexionApacheDerby {
 
             // Verificar si la conexión fue exitosa
             if (conexion.getConnection() != null && !conexion.getConnection().isClosed()) {
-                System.out.println("Conexión establecida correctamente con la base de datos Oracle.");
+                System.out.println("Conexión establecida correctamente con la base de datos Apache Derby.");
             } else {
                 System.err.println("No se ha podido establecer la conexión con la base de datos.");
             }
