6d40c9dea3fdc21474c60b680969fa3b698e6b9e
telcaslor <thatbytc@gmail.com>
2025-10-17 14:17:13 +0200

    WIP: creado el modalEditar y el esqueleto ControladorEditar

---
+++ src/main/java/es/potter/control/ControladorEditar.java
@@ -0,0 +1,35 @@
+package es.potter.control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ControladorEditar {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnCancelar1;
+
+    @FXML
+    private ComboBox<?> cmbxCurso;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtPatronus;
+
+    @FXML
+    void actionCancelar(ActionEvent event) {
+
+    }
+
+}
+
