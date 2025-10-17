6497c7b262d7fc14a927548aa5d17b35f39717f0
telcaslor <thatbytc@gmail.com>
2025-10-17 13:39:58 +0200

    WIP: ventana modal de insercion de alumnos y esqueleto controlador para el modal creado.

---
+++ src/main/java/es/potter/control/ControladorModal.java
@@ -0,0 +1,41 @@
+package es.potter.control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControladorModal {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private TextField txtCasa;
+
+    @FXML
+    private TextField txtCurso;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtPatronus;
+
+    @FXML
+    void actionCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionGuardar(ActionEvent event) {
+
+    }
+
+}
